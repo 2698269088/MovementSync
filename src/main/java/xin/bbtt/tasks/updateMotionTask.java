3b6fe490dc0b43b92903c333596edf15aceb9620
huangdihd <hd20100104@163.com>
2025-12-12 23:51:29 +0800

    Refactor ground check and motion update logic for improved accuracy

--- src/main/java/xin/bbtt/tasks/updateMotionTask.java
+++ src/main/java/xin/bbtt/tasks/updateMotionTask.java
@@ -5,6 +5,7 @@ import org.joml.Vector3d;
 import xin.bbtt.MovementSync;
 import xin.bbtt.mcbot.Bot;
 import xin.bbtt.mcbot.Server;
+import xin.bbtt.world.Direction;
 
 import static xin.bbtt.MovementSync.gravitationalAcceleration;
 import static xin.bbtt.MovementSync.terminalVelocity;
@@ -27,12 +28,17 @@ public class updateMotionTask implements Runnable {
         if (!Bot.Instance.isRunning()) return;
         if (Bot.Instance.getServer() != Server.Xin) return;
         Vector3d lastPos = new Vector3d(MovementSync.Instance.position.get());
-        checkOnGround();
-
         Vector3d velocity = MovementSync.Instance.velocity.get();
         Vector3d displacement = new Vector3d();
+
+        checkOnGround();
+        if(MovementSync.Instance.onGround.get()) {
+            velocity.y = 0;
+            displacement.y = 0;
+        }
+
         if (velocity.y > terminalVelocity) {
-            velocity.add(gravitationalAcceleration);
+            if (!MovementSync.Instance.onGround.get()) velocity.add(gravitationalAcceleration);
             velocity.y *= 0.9800000190734863D;
             displacement.add(MovementSync.Instance.velocity.get());
             displacement.add(new Vector3d(gravitationalAcceleration).div(2));
@@ -40,13 +46,20 @@ public class updateMotionTask implements Runnable {
             velocity.y = terminalVelocity;
             displacement.add(MovementSync.Instance.velocity.get().add(velocity).div(2));
         }
-
-        if(MovementSync.Instance.onGround.get()) {
-            velocity.y = 0;
-            displacement.y = 0;
-        }
         Vector3d position = new Vector3d(MovementSync.Instance.position.get());
+        Vector3d lowest = new Vector3d(MovementSync.Instance.position.get()).add(Direction.DOWN.getUnitVector());
+
+        if (!MovementSync.Instance.onGround.get()) {
+            while (!isOnGround(lowest))
+                lowest.add(Direction.DOWN.getUnitVector());
+        }
+
         position.add(displacement);
+
+        if (position.y < lowest.y){
+            position.y = lowest.y;
+        }
+
         MovementSync.Instance.velocity.set(velocity);
         MovementSync.Instance.position.set(position);
 
