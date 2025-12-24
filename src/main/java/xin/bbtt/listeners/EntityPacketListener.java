e27095682fb5f6554c4a22a10744188216f32187
huangdihd <hd20100104@163.com>
2025-12-25 00:59:01 +0800

    Update xinbot dependency to 1.13.3-RELEASE and remove redundant type checks in EntityPacketListener

--- src/main/java/xin/bbtt/listeners/EntityPacketListener.java
+++ src/main/java/xin/bbtt/listeners/EntityPacketListener.java
@@ -11,43 +11,31 @@ public class EntityPacketListener implements Listener {
 
     @EventHandler
     public void OnAddEntities(ReceivePacketEvent<ClientboundAddEntityPacket> receivePacketEvent) {
-        //noinspection DataFlowIssue
-        if (!(receivePacketEvent.getPacket() instanceof ClientboundAddEntityPacket)) return;
         World.Instance.handleAddEntityPacket(receivePacketEvent.getPacket());
     }
 
     @EventHandler
     public void OnMoveEntityPosition(ReceivePacketEvent<ClientboundMoveEntityPosPacket> receivePacketEvent) {
-        //noinspection DataFlowIssue
-        if (!(receivePacketEvent.getPacket() instanceof ClientboundMoveEntityPosPacket)) return;
         World.Instance.handleMoveEntityPosPacket(receivePacketEvent.getPacket());
     }
 
     @EventHandler
     public void OnMoveEntityRotation(ReceivePacketEvent<ClientboundMoveEntityRotPacket> receivePacketEvent) {
-        //noinspection DataFlowIssue
-        if (!(receivePacketEvent.getPacket() instanceof ClientboundMoveEntityRotPacket)) return;
         World.Instance.handleMoveEntityRotPacket(receivePacketEvent.getPacket());
     }
 
     @EventHandler
     public void OnMoveEntityPositronRotation(ReceivePacketEvent<ClientboundMoveEntityPosRotPacket> receivePacketEvent) {
-        //noinspection DataFlowIssue
-        if (!(receivePacketEvent.getPacket() instanceof ClientboundMoveEntityPosRotPacket)) return;
         World.Instance.handleMoveEntityPosRotPacket(receivePacketEvent.getPacket());
     }
 
     @EventHandler
     public void OnMoveEntityRotateHead(ReceivePacketEvent<ClientboundRotateHeadPacket> receivePacketEvent) {
-        //noinspection DataFlowIssue
-        if (!(receivePacketEvent.getPacket() instanceof ClientboundRotateHeadPacket)) return;
         World.Instance.handleRotateHeadPacket(receivePacketEvent.getPacket());
     }
 
     @EventHandler
     public void OnRemoveEntity(ReceivePacketEvent<ClientboundRemoveEntitiesPacket> receivePacketEvent) {
-        //noinspection DataFlowIssue
-        if (!(receivePacketEvent.getPacket() instanceof ClientboundRemoveEntitiesPacket)) return;
         World.Instance.handleRemoveEntitiesPacket(receivePacketEvent.getPacket());
     }
 }
